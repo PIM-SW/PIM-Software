23a4ee9cf063fdcd06826304d111df748d66078b
root <heelim217@gmail.com>
2023-01-09 21:56:55 +0000

    File added : test scripts for frontend, midend, backend code generation

---
+++ test/frontend.sh
@@ -0,0 +1,2 @@
+../Polygeist/build/mlir-clang/mlir-clang --pim-avail -S ../PIM_BLAS/src/cblas_saxpy.c -function=cblas_saxpy;
+../Polygeist/build/mlir-clang/mlir-clang --pim-avail -S ../PIM_BLAS/src/cblas_saxpy.c -function=cblas_saxpy > ./saxpy.mlir
